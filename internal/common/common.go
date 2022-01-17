57be757a4980508f86a6ff7c83757e77c1fcf010
IvanBarayev <ivanbarayev@hotmail.com>
2022-01-18 02:33:44 +0300

    Added HTTP client for sending data to Nodejs API

--- internal/common/common.go
+++ internal/common/common.go
@@ -2,8 +2,10 @@ package common
 
 import (
 	"fmt"
+	"io/ioutil"
 	ent "main/internal/entities"
 	"math/rand"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
@@ -54,11 +56,42 @@ func BGGenerator(index int) {
 	RangeIndex := fmt.Sprintf("SENSOR%d_RANGE", index)
 	for _ = range ticker.C {
 		SensorData = NumGenerator(os.Getenv(RangeIndex))
+		PostToAPI(index, SensorData)
 		fmt.Printf("Sensor %d Data : %d \n", index, SensorData)
 	}
 }
 
-func PostToAPI(uri string) int {
-	//TODO HTTP CLIENT native
+func PostToAPI(index int, sensor_data int) int {
+	PathIndex := fmt.Sprintf("SENSOR%d_PATH", index)
+	URL := fmt.Sprintf("http://%s:%s/%s", os.Getenv("MAIN_API_URI"), os.Getenv("MAIN_API_PORT"), os.Getenv(PathIndex))
+	println(URL)
+	MethodIndex := fmt.Sprintf("SENSOR%d_METHOD", index)
+	Method := os.Getenv(MethodIndex)
+	Data := fmt.Sprintf(`{"status": 1,"id": %d,"sensor_data": %d,"time_stamp": %d}`, index, sensor_data, time.Now().UnixMilli())
+	fmt.Printf("DATAAAAA : %s", Data)
+	payload := strings.NewReader(Data)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(Method, URL, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+	fmt.Println(string(body))
 	return 1
 }
